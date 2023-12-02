c1eabf9c4f52022168fb44ec32f940affde9024b
István Juhos <stewemetal@gmail.com>
2023-12-03 00:29:28 +0100

    Add solution for Day02_2

--- src/Day02.kt
+++ src/Day02.kt
@@ -19,13 +19,33 @@ fun main() {
         return result
     }
 
+    fun powerOfMaxDrawsPerColor(draws: String): Int =
+            draws.trim().split(";", ",").map { drawRaw ->
+                val (countRaw, color) = drawRaw.trim().split(" ")
+                Pair(color, countRaw.toInt())
+            }.groupBy { it.first }
+                    .map { entry ->
+                        entry.value.maxBy { it.second }
+                    }.fold(0) { aggregate, currentValue ->
+                        if (aggregate == 0) {
+                            currentValue.second
+                        } else {
+                            aggregate * currentValue.second
+                        }
+                    }
+
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.fold(0) { result, game ->
+            val (_, draws) = game.split(":")
+
+            result + powerOfMaxDrawsPerColor(draws)
+        }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day02_test")
     check(part1(testInput) == 8)
+    check(part2(testInput) == 2286)
 
     val input = readInput("Day02")
     part1(input).println()
