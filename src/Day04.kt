2c317202b33cbf863722ca29a3c29eee66933051
István Juhos <stewemetal@gmail.com>
2023-12-11 13:01:32 +0100

    Add solution for Day04_2

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,11 +1,14 @@
 import kotlin.math.pow
 
 fun main() {
+    fun String.extractNumbers(): List<Int> =
+        trim().split("\\s+".toRegex()).map { it.toInt() }
+
     fun part1(input: List<String>): Int {
         return input.fold(0) { sum, line ->
             val (_, rawCardNumbers, rawWinningNumbers) = line.split(" | ", ": ")
-            val cardNumbers = rawCardNumbers.trim().split("\\s+".toRegex()).map { it.toInt() }
-            val winningNumbers = rawWinningNumbers.trim().split("\\s+".toRegex()).map { it.toInt() }
+            val cardNumbers = rawCardNumbers.extractNumbers()
+            val winningNumbers = rawWinningNumbers.extractNumbers()
 
 
             val numbersMatching = cardNumbers.intersect(winningNumbers.toSet()).size
@@ -18,14 +21,36 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        var sum = 0
-        return sum
+        val scratchcards = mutableMapOf<Int, Int>()
+        val scratchcardPile = mutableMapOf<Int, Int>()
+        input.forEach { line ->
+            val (rawCardId, rawCardNumbers, rawWinningNumbers) = line.split(" | ", ": ")
+            val cardId = rawCardId.split("\\s+".toRegex())[1].toInt()
+            val cardNumbers = rawCardNumbers.extractNumbers()
+            val winningNumbers = rawWinningNumbers.extractNumbers()
+
+            val numbersMatching = cardNumbers.intersect(winningNumbers.toSet()).size
+
+            scratchcards[cardId] = numbersMatching
+            scratchcardPile[cardId] = 1
+        }
+
+        scratchcards.forEach { (cardId, value) ->
+            repeat(scratchcardPile.getValue(cardId)) {
+                repeat(value) { current ->
+                    val wonCardId = cardId + current + 1
+                    scratchcardPile[wonCardId] = (scratchcardPile[wonCardId] ?: 0) + 1
+                }
+            }
+        }
+
+        return scratchcardPile.values.sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day04_test")
     check(part1(testInput) == 13)
-    check(part2(testInput) == 0)
+    check(part2(testInput) == 30)
 
     val input = readInput("Day04")
     part1(input).println()
