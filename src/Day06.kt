7098058275712d741a5fe9e12f1c284e2ea5651b
István Juhos <stewemetal@gmail.com>
2023-12-26 01:15:39 +0100

    Add solution for Day06_2

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,5 +1,5 @@
 fun main() {
-    fun part1(input: List<String>): Int =
+    fun part1(input: List<String>): Long =
             input
                     .readRaces()
                     .map { race ->
@@ -8,15 +8,15 @@ fun main() {
                         accumulator * current
                     }
 
-    fun part2(input: List<String>): Int {
-        var sum = 0
-        return sum
-    }
+    fun part2(input: List<String>): Long =
+            input
+                    .readRace()
+                    .getNumberOfWinningCases()
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day06_test")
-    check(part1(testInput) == 288)
-    check(part2(testInput) == 0)
+    check(part1(testInput) == 288L)
+    check(part2(testInput) == 71503L)
     part2(testInput)
 
     val input = readInput("Day06")
@@ -25,14 +25,14 @@ fun main() {
 }
 
 private data class Race(
-        val time: Int,
-        val distance: Int,
+        val time: Long,
+        val distance: Long,
 )
 
 private fun List<String>.readRaces(): List<Race> {
     val whitespacesRegex = "\\s+".toRegex()
-    val times = this[0].split(":\\s+".toRegex())[1].split(whitespacesRegex).map { it.toInt() }
-    val distances = this[1].split(":\\s+".toRegex())[1].split(whitespacesRegex).map { it.toInt() }
+    val times = this[0].split(":\\s+".toRegex())[1].split(whitespacesRegex).map { it.toLong() }
+    val distances = this[1].split(":\\s+".toRegex())[1].split(whitespacesRegex).map { it.toLong() }
     return buildList {
         times.forEachIndexed { index, time ->
             add(Race(time, distances[index]))
@@ -40,8 +40,15 @@ private fun List<String>.readRaces(): List<Race> {
     }
 }
 
-private fun Race.getNumberOfWinningCases(): Int {
-    var numberOfCases = 0
+private fun List<String>.readRace(): Race {
+    val whitespacesRegex = "\\s+".toRegex()
+    val time = this[0].split(":\\s+".toRegex())[1].replace(whitespacesRegex, "").toLong()
+    val distance = this[1].split(":\\s+".toRegex())[1].replace(whitespacesRegex, "").toLong()
+    return Race(time, distance)
+}
+
+private fun Race.getNumberOfWinningCases(): Long {
+    var numberOfCases = 0L
     (1 until time).forEach { buttonHoldingTime ->
         if ((time - buttonHoldingTime) * buttonHoldingTime > distance) {
             numberOfCases++
