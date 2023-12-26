607f9914bbe337578075ef4b34c90e6a8f1d1bcf
István Juhos <stewemetal@gmail.com>
2023-12-26 20:33:07 +0100

    Add solution for Day07_2

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,44 +1,87 @@
+import HandType.FIVE_OF_A_KIND
 import java.lang.Exception
 
 fun main() {
     fun part1(input: List<String>): Int =
             input.readHands()
-                    .sorted()
+                    .sortedWith(
+                            HandComparator(
+                                    mapOf(
+                                            'A' to 'a',
+                                            'K' to 'b',
+                                            'Q' to 'c',
+                                            'J' to 'd',
+                                            'T' to 'e',
+                                            '9' to 'f',
+                                            '8' to 'g',
+                                            '7' to 'h',
+                                            '6' to 'i',
+                                            '5' to 'j',
+                                            '4' to 'k',
+                                            '3' to 'l',
+                                            '2' to 'm',
+                                    ),
+                            ),
+                    )
                     .mapIndexed { index, hand ->
                         (index + 1) * hand.bid
                     }
                     .sum()
 
-    fun part2(input: List<String>): Int {
-        var sum = 0
-        return sum
-    }
+    fun part2(input: List<String>): Int =
+            input.readHandsWithJokers()
+                    .sortedWith(
+                            HandComparator(
+                                    mapOf(
+                                            'A' to 'a',
+                                            'K' to 'b',
+                                            'Q' to 'c',
+                                            'T' to 'd',
+                                            '9' to 'e',
+                                            '8' to 'f',
+                                            '7' to 'g',
+                                            '6' to 'h',
+                                            '5' to 'i',
+                                            '4' to 'j',
+                                            '3' to 'k',
+                                            '2' to 'l',
+                                            'J' to 'm',
+                                    ),
+                            ),
+                    )
+                    .mapIndexed { index, hand ->
+                        (index + 1) * hand.bid
+                    }
+                    .sum()
+
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day07_test")
     check(part1(testInput) == 6440)
-    check(part2(testInput) == 0)
+    check(part2(testInput) == 5905)
 
     val input = readInput("Day07")
     part1(input).println()
     part2(input).println()
 }
 
-private val cardStrengths = mapOf(
-        'A' to 'a',
-        'K' to 'b',
-        'Q' to 'c',
-        'J' to 'd',
-        'T' to 'e',
-        '9' to 'f',
-        '8' to 'g',
-        '7' to 'h',
-        '6' to 'i',
-        '5' to 'j',
-        '4' to 'k',
-        '3' to 'l',
-        '2' to 'm',
-)
+private class HandComparator(
+        val cardStrengths: Map<Char, Char>,
+) : Comparator<Hand> {
+
+    override fun compare(o1: Hand, o2: Hand): Int {
+        return if (o1.type.strength != o2.type.strength) {
+            o1.type.strength.compareTo(o2.type.strength)
+        } else {
+            o2.mapCardsToStrengths().compareTo(o1.mapCardsToStrengths())
+        }
+    }
+
+    private fun Hand.mapCardsToStrengths() =
+            cards.map { it.asCardStrength() }.joinToString()
+
+    private fun Char.asCardStrength() = cardStrengths.getValue(this)
+}
 
 private enum class HandType(val strength: Int) {
     HIGH_CARD(0),
@@ -54,20 +97,7 @@ private data class Hand(
         val cards: String,
         val bid: Int,
         val type: HandType,
-) : Comparable<Hand> {
-    override fun compareTo(other: Hand): Int {
-        return if (type.strength != other.type.strength) {
-            type.strength.compareTo(other.type.strength)
-        } else {
-            other.mapCardsToStrengths().compareTo(mapCardsToStrengths())
-        }
-    }
-
-    private fun Hand.mapCardsToStrengths() =
-            cards.map { it.cardStrength() }.joinToString()
-
-    private fun Char.cardStrength() = cardStrengths.getValue(this)
-}
+)
 
 private fun List<String>.readHands() =
         map {
@@ -76,6 +106,16 @@ private fun List<String>.readHands() =
                     cards = hand,
                     bid = bid.toInt(),
                     type = hand.toHandType()
+            )
+        }
+
+private fun List<String>.readHandsWithJokers() =
+        map {
+            val (hand, bid) = it.split(" ")
+            Hand(
+                    cards = hand,
+                    bid = bid.toInt(),
+                    type = hand.toHandTypeWithJokers()
             )
         }
 
@@ -121,7 +161,22 @@ private fun String.toHandType(): HandType {
         isThreeOfAKind() -> HandType.THREE_OF_A_KIND
         isFullHouse() -> HandType.FULL_HOUSE
         isFourOfAKind() -> HandType.FOUR_OF_A_KIND
-        isFiveOfAKind() -> HandType.FIVE_OF_A_KIND
+        isFiveOfAKind() -> FIVE_OF_A_KIND
         else -> throw Exception("Invalid hand type: $this")
+    }
+}
+
+private fun String.toHandTypeWithJokers(): HandType {
+
+    val cardWithMostCountWithoutJoker = groupBy { it }
+            .toList()
+            .filter { it.first != 'J' }
+            .sortedByDescending { it.second.size }.getOrNull(0)
+            ?.first
+
+    return if (cardWithMostCountWithoutJoker != null) {
+        this.replace('J', cardWithMostCountWithoutJoker).toHandType()
+    } else {
+        FIVE_OF_A_KIND
     }
 }
